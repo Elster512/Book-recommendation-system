15228c3c1f8d59270524d56392a1495f0c8cfe8c
Alexander <andrey.kgokhloff2013@gmail.com>
2023-11-08 10:50:16 +0300

    removed App.css import

--- web-recomendation/src/App.tsx
+++ web-recomendation/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+
 import Layout from './components/Layout/Layout/Layout';
 import Home from './pages/Home/Home';
 import SingleBookPage from './pages/SingleBookPage/SingleBookPage';
--- web-recomendation/src/main.tsx
+++ web-recomendation/src/main.tsx
@@ -1,11 +1,11 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
