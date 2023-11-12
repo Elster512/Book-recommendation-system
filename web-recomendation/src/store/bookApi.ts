ecb841abb997ba5a9434222fcdfdddb7860cee37
Alexandr <andrey.kgokhloff2013@gmail.com>
2023-11-12 16:46:07 +0300

    new url for api

--- web-recomendation/src/store/bookApi.ts
+++ web-recomendation/src/store/bookApi.ts
@@ -1,36 +1,36 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { Book } from '../types/bookcard';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { Book } from "../types/bookcard";
 
-const BASE_URL = 'http://127.1.0.1:8000';
+const BASE_URL = "https://bookapu.onrender.com";
 export const booksApi = createApi({
-  reducerPath: 'booksApi',
+  reducerPath: "booksApi",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
-    credentials: 'same-origin',
+    credentials: "same-origin",
   }),
-  tagTypes: ['Books', 'Book', 'Rec', 'User'],
+  tagTypes: ["Books", "Book", "Rec", "User"],
   endpoints: (builder) => ({
     getBooks: builder.query<
       { books: Book[]; pages: number; page: number },
       string
     >({
       query: (page) => `books?${page && `page=${page}`}`,
-      providesTags: () => ['Books'],
+      providesTags: () => ["Books"],
     }),
     getSingleBook: builder.query<{ book: Book }, string | undefined>({
       query: (id) => `books/${id}`,
-      providesTags: () => ['Book'],
+      providesTags: () => ["Book"],
     }),
     getRecBooks: builder.query<{ rec_books: Book[] }, void>({
-      query: () => 'rec_user',
-      providesTags: () => ['Books', 'Rec', 'User'],
+      query: () => "rec_user",
+      providesTags: () => ["Books", "Rec", "User"],
     }),
     getRecBooksForBook: builder.query<
       { rec_books: Book[] },
       string | undefined
     >({
       query: (id) => `rec_book?${id && `title=${id}`}`,
-      providesTags: () => ['Books', 'Book', 'Rec'],
+      providesTags: () => ["Books", "Book", "Rec"],
     }),
   }),
 });
