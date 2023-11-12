ad818158888047d317492507a2af10c3df489d77
Alexandr <andrey.kgokhloff2013@gmail.com>
2023-11-12 17:07:14 +0300

    fixed adapt

--- web-recomendation/src/components/Layout/Header/Header.tsx
+++ web-recomendation/src/components/Layout/Header/Header.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography } from '@mui/material';
-import BookIcon from '@mui/icons-material/Book';
-import s from './Header.module.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import BookIcon from "@mui/icons-material/Book";
+import s from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
-    <AppBar position="static" sx={{ width: '100%', padding: '10px' }}>
+    <AppBar position="static" sx={{ width: "100%", padding: "10px" }}>
       <Toolbar>
-        <Link to="/" className={s['header-link']}>
-          <BookIcon sx={{ fontSize: '40px', flexGrow: 0, display: 'flex' }} />
+        <Link to="/" className={s["header-link"]}>
+          <BookIcon sx={{ fontSize: "40px", flexGrow: 0, display: "flex" }} />
           <Typography
             variant="h5"
             sx={{
               fontWeight: 800,
-              fontFamily: 'sans-serif',
+              fontFamily: "sans-serif",
               flexGrow: 1,
-              textDecoration: 'unset',
+              textDecoration: "unset",
             }}
           >
             BookLife.com
