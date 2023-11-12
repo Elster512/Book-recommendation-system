596780380a68599d6aa068ab9a989c7539dbc239
Alexandr <andrey.kgokhloff2013@gmail.com>
2023-11-12 16:55:56 +0300

    fixed carousel 2

--- web-recomendation/src/components/Carousel/BooksCarousel/queries.ts
+++ web-recomendation/src/components/Carousel/BooksCarousel/queries.ts
@@ -1,4 +1,4 @@
 export const queryLg = "(max-width:1200px)";
 export const queryMd = "(max-width:930px)";
 export const querySm = "(max-width:690px)";
-export const queryXs = "(max-width:4600px)";
+export const queryXs = "(max-width:460px)";
