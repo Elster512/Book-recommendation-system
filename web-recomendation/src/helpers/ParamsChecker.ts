9968c3b220e817cb4a0cdccca7df24368db6f7c5
Alexandr <andrey.kgokhloff2013@gmail.com>
2023-11-26 11:21:15 +0300

    cant use negative numbers

--- web-recomendation/src/helpers/ParamsChecker.ts
+++ web-recomendation/src/helpers/ParamsChecker.ts
@@ -4,6 +4,9 @@ export const pageChecker = (page: string | null) => {
   if (!page) {
     return false;
   }
+  if (Number(page) <= 0) {
+    return false;
+  }
   if (!page.match(pageReg)) {
     return false;
   }
