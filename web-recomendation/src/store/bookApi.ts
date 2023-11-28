483469a5f3c41259e0d86ba69895aaaad4a71a5f
Alexandr <andrey.kgokhloff2013@gmail.com>
2023-11-28 17:52:18 +0300

    changed api url

--- web-recomendation/src/store/bookApi.ts
+++ web-recomendation/src/store/bookApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Book } from "../types/bookcard";
 import { Query } from "../types/query";
 
-const BASE_URL = "https://bookapu.onrender.com";
+const BASE_URL = "https://localhost:8008/";
 export const booksApi = createApi({
   reducerPath: "booksApi",
   baseQuery: fetchBaseQuery({
