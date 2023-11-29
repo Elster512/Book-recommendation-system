df95d6790da313a77927e57a0ea307ec7ded7c9e
Alexandr <andrey.kgokhloff2013@gmail.com>
2023-11-29 23:22:40 +0300

    dasa

--- web-recomendation/src/store/bookApi.ts
+++ web-recomendation/src/store/bookApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Book } from "../types/bookcard";
 import { Query } from "../types/query";
 
-const BASE_URL = "http://localhost:8000/";
+const BASE_URL = "http://localhost:8000/api/";
 export const booksApi = createApi({
   reducerPath: "booksApi",
   baseQuery: fetchBaseQuery({
